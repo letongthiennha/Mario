989295f596bbacbb8af25aa7b5799921ebdd5a9a
nguyendinhthienloc <dinhthienlocnguyen@gmail.com>
2025-05-30 12:44:03 +0700

    Grammar shit

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -82,7 +82,7 @@ void ResourceManager::loadTextures()
     textures["MAP1_GRASS_RIGHT_EDGE"] = LoadTexture("resources/Entity/Tiles/Map 1/tile_E.png");
     textures["MAP1_GRASS_LEFT_EDGE"] = LoadTexture("resources/Entity/Tiles/Map 1/tile_F.png");
     }
-    //MONSTER
+    //MONSTERS
     {
     textures["GOOMBA_0_RIGHT"] = LoadTexture("resources/Entity/Monsters/Goomba/Goomba_0.png");
     textures["GOOMBA_0_LEFT"] = FlipTextureHorizontal(textures["GOOMBA_0_RIGHT"]);
