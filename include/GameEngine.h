b404dcc71a38437738d428660c56a15abc070907
nguyendinhthienloc <dinhthienlocnguyen@gmail.com>
2025-05-30 12:37:29 +0700

    Update GameEngine

--- include/GameEngine.h
+++ include/GameEngine.h
@@ -1,32 +1,24 @@
-/* File: ../include/GameEngine.h */
 #ifndef GAMEENGINE_H
 #define GAMEENGINE_H
-#include "raylib.h"
-#include "Mario.h"
 #include "World.h"
-#include "ResourceManager.h"
-#include "GameClock.h"
-#include "Monster.h"
+#include "raylib.h"
 #include "Goomba.h"
-#include <vector>
 
 class GameEngine {
-private:
-    World* world;
-    Mario* player;
-    std::vector<Monster*> monsters;
-    bool isPaused;
-    bool isGameOver;
-    int screenWidth;
-    int screenHeight;
-    float startDelay; // Delay for initial collision checks
 public:
     GameEngine(int width, int height);
     ~GameEngine();
-    void spawnMonsters();
     void update();
     void draw();
     void run();
+private:
+    int screenWidth;
+    int screenHeight;
+    bool isPaused;
+    bool isGameOver;
+    float startDelay;
+    World* world;
+    Mario* player;
+    void spawnMonsters();
 };
-
-#endif // GAMEENGINE_H
+#endif
