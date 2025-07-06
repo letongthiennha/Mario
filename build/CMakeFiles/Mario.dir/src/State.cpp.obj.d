917e835267d6f3a992fdc69ddb7725f2011e7217
Lê Tống Thiện Nhân <letongthiennha@gmail.com>
2025-07-06 18:33:09 +0700

    Remove build folder

--- build/CMakeFiles/Mario.dir/src/State.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Mario.dir/src/State.cpp.obj: \
- C:\Users\ADMIN\My-Mario\Mario\src\State.cpp \
- C:/Users/ADMIN/My-Mario/Mario/include/State.h \
- C:/Users/ADMIN/My-Mario/Mario/include/StateManager.h
