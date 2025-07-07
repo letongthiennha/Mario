899e64dcc2ddda5ccadb0f79c8dfec547305e8ac
tuankhai2006 <vankhacle2006@gmail.com>
2025-07-07 14:43:08 +0700

    add sound controller functions

--- include/SoundControoler.h
+++ include/SoundControoler.h
@@ -12,6 +12,13 @@ class SoundController {
     SoundController& operator=(const SoundController&) = delete;
     std::unordered_map<std::string, Sound*> registeredSounds;
     std::unordered_map<std::string, Music*> registeredMusics;
+
+    float sfxVolume = 1.0f;
+    float musicVolume = 1.0f;
+    float prevMusicVolume = 1.0f;
+    float prevSFXVolume = 1.0f;
+    bool musicMuted = false;
+    bool sfxMuted = false;
 public:
     static SoundController &getInstance();
     void PlaySound(const std::string&);
@@ -20,4 +27,16 @@ public:
     void PauseMusic(const std::string&);
     void StopAllSounds();
     void UpdateSoundStream();
+
+    void SetSFXVolume(float volume);
+    void SetMusicVolume(float volume);
+    float GetSFXVolume() const;
+    float GetMusicVolume() const;
+
+    void MuteMusic();
+    void UnmuteMusic();
+    void MuteSFX();
+    void UnmuteSFX();
+    bool IsMusicMuted() const;
+    bool IsSFXMuted() const;
 };
