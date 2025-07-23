2acff3bc9abef02fca489b2c6ad636575cfe73f7
nguyendinhthienloc <dinhthienlocnguyen@gmail.com>
2025-07-23 09:10:25 +0700

    Update Rex.h

--- include/Rex.h
+++ include/Rex.h
@@ -7,7 +7,9 @@ public:
     void updateSprite() override;
     void updateStateAndPhysic() override;
     void handleCollision(const Tile& tile, CollisionInfo type) override;
-
+    void die() override;
+    int getHitsToDie() const { return hitsToDie; }
+    float getSpeed() const { return speed; }
 private:
     int hitsToDie = 2;
     float dyingFrameAcum = 0;
@@ -15,4 +17,5 @@ private:
     int currentDyingFrame = 0;
     const int maxDyingFrames = 4;
     Vector2 posOnDying;
+    float hitCooldown; // Cooldown after being hit
 };
